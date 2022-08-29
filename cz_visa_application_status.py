24e96b0117e5b63e9727a4599517450f09ea6528
maxnikulcha96 <maxnikulcha96@gmail.com>
2022-08-29 11:54:25 +0200

    Now cz visa screenshot saved into provided path

--- cz_visa_application_status.py
+++ cz_visa_application_status.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from datetime import datetime
 import sys
 from framework.checkers.checkers import BaseWebsiteChecker
 from framework.web.browsers import FirefoxBrowser
@@ -13,11 +14,12 @@ def setUp():
     website = MoiApplicationStatus(browser)
     checker = BaseWebsiteChecker(website)
 
-    global person_name, application_number, application_type, application_year
+    global person_name, application_number, application_type, application_year, screenshot_path
     person_name = str(sys.argv[1])
     application_number = str(sys.argv[2])
     application_type = str(sys.argv[3])
     application_year = str(sys.argv[4])
+    screenshot_path = str(sys.argv[5])
 
 
 def get_application_status():
@@ -35,6 +37,9 @@ def get_application_status():
 
     print("{0}, the status of your application 'OAM-{1}/{2}-{3}' is '{4}'".format(
         person_name, application_number, application_type, application_year, status))
+
+    browser.take_screenshot("{0}/{1}_OAM-{2}_{3}_{4}_{5}_{6}.png".format(
+        screenshot_path, person_name, application_number, application_type, application_year, status, datetime.now()))
 
 
 def tearDown():
