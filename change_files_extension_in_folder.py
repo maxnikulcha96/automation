d1413954436e74073c67ca10d3e1941b8c5730b0
maxnikulcha96 <maxnikulcha96@gmail.com>
2023-07-20 13:42:37 +0200

    Add changes files extension

---
+++ change_files_extension_in_folder.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+
+def setUp():
+    global dir, extension
+
+    # dir = '/Users/maximnikulcha/Downloads/gifs/'
+    # extension = '.gif'
+    dir = str(sys.argv[1])
+    extension = str(sys.argv[2])
+
+
+def change():
+    for filename in os.listdir(dir):
+        full_path = dir + filename
+        base = os.path.splitext(full_path)[0]
+
+        if (extension in full_path): 
+            os.rename(full_path, base)
+        else:
+            os.rename(full_path, base + extension)
+
+def main():
+    setUp()
+    change()
+
+if __name__ == "__main__":
+    main()
